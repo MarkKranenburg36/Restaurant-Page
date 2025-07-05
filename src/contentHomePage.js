4a50c23c4e9cf6299b4593b9c5df25ab4bb70ee2
Mark Kranenburg <MarkKranenburg36@users.noreply.github.com>
2025-07-05 17:47:45 +0200

    add layout for menu

--- src/contentHomePage.js
+++ src/contentHomePage.js
@@ -7,17 +7,17 @@ export default function renderContentHomePage() {
 
     const img = document.createElement('img');
     img.src = chefImg;
-    img.classList.add('contentWith');
-    contentContainer.appendChild(img);
 
-    
     const heroHeader = document.createElement('h1');
     heroHeader.innerText = 'Welkom bij Chopstick Cheff - waar Aziatische smaken tot leven komen.'
-    heroHeader.classList.add('contentWith');
-    contentContainer.appendChild(heroHeader)
 
     const heroParagraph = document.createElement('p');
     heroParagraph.innerText = `Laat je meevoeren op een culinaire reis door het Verre Oosten, met authentieke gerechten, moderne twist en de passie van onze chefs in elke hap. Van dampende ramen tot knapperige wokgerechten - bij Chopstick Cheff proef je de kunst van de Aziatische keuken. Pak je chopsticks erbij... en ontdek jouw nieuwe favoriete smaken.`;
-    heroParagraph.classList.add('contentWith');
-    contentContainer.appendChild(heroParagraph);
+
+    const elements = [img, heroHeader, heroParagraph];
+
+    elements.forEach(el => {
+        el.classList.add('contentWith');
+        contentContainer.appendChild(el);
+    });
 }
--- src/contentMenuPage.js
+++ src/contentMenuPage.js
@@ -1,4 +1,32 @@
 export default function renderContentMenuPage() {
     const contentContainer = document.getElementById('contentContainer');
     contentContainer.innerHTML = ``;
+    contentContainer.classList.add('centered');
+
+    const menuHeader = document.createElement('h1');
+    menuHeader.innerText = 'Menu';
+
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menuGrid');
+
+    const menuSubtitles = ['VOORGERECHTEN', 'NOEDELGERECHTEN', 'RUNDVLEESGERECHTEN'];
+    for(let i = 0; i < menuSubtitles.length; i++) {
+        const subtitle = document.createElement('h2');
+        subtitle.innerText = menuSubtitles[i];
+        if (subtitle.innerText === 'RUNDVLEESGERECHTEN') { subtitle.classList.add('beefSubheader') }
+        menuContainer.appendChild(subtitle);
+    }
+    
+
+    const appetzersContainer = document.createElement('p');
+    appetzersContainer.classList.add('appetizers');
+    const listAppetizers = ['LOEMPIA (4 STUKS)', 'LOEMPIA MET KIP (2 STUKS)', 'KRAB WONTONS (4 STUKS)', 'CHICKEN WINGS (6 STUKS)', 'CHICKEN SATE (4 STUKS)'];
+    listAppetizers.forEach(el => appetzersContainer.appendChild(el));
+    menuContainer.appendChild(appetzersContainer);
+
+    const elements = [menuHeader, menuContainer];
+    elements.forEach(el => {
+        el.classList.add('contentWidth');
+        contentContainer.appendChild(el);
+    });
 }
