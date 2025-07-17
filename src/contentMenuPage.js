a3b35d8bd17e3e7d475a292b0c92b963d5fa0b40
Mark Kranenburg <MarkKranenburg36@users.noreply.github.com>
2025-07-17 22:23:24 +0200

    add renderItemListToMenuContainer function to render the menu lists of dishes

--- src/contentMenuPage.js
+++ src/contentMenuPage.js
@@ -18,14 +18,31 @@ export default function renderContentMenuPage() {
         if (subtitle.innerText === 'RUNDVLEESGERECHTEN') { subtitle.classList.add('beefSubheader') }
         menuContainer.appendChild(subtitle);
     }
-    
 
-    const appetizersContainer = document.createElement('div');
-    appetizersContainer.classList.add('appetizers');
-    appetizersContainer.classList.add('menuItemList');
-    const listAppetizers = ['LOEMPIA (4 STUKS)', '9.50', 'LOEMPIA MET KIP (2 STUKS)', '8.00', 'KRAB WONTONS (4 STUKS)', '7.00', 'CHICKEN WINGS (6 STUKS)', '9.00', 'CHICKEN SATE (4 STUKS)', '10.00'];
-    appetizersContainer.innerHTML = listAppetizers.map(el => `<p>${el}</p>`).join('');
-    menuContainer.appendChild(appetizersContainer);
+    function renderItemListToMenuContainer(items, layoutClass) {
+        const container = document.createElement('div');
+        container.classList.add('menuItemList');
+        container.classList.add(layoutClass);
+
+        container.innerHTML = items.map(el => `<p>${el}</p>`).join('');
+        menuContainer.appendChild(container);
+    }
+
+    const listAppetizers = [
+        'LOEMPIA (4 STUKS)', '9.50',
+        'LOEMPIA MET KIP (2 STUKS)', '8.00',
+        'KRAB WONTONS (4 STUKS)', '7.00',
+        'CHICKEN WINGS (6 STUKS)', '9.00',
+        'CHICKEN SATE (4 STUKS)', '10.00'
+    ];
+
+    const listNoodleDishes = [
+        'NOEDELS MET GROENTEN', '10.00',
+        'NOEDELS MET KIP', '12.00',
+        'NOEDELS MET GARNALEN', '12.00',
+    ];
+
+    renderItemListToMenuContainer(listAppetizers, 'appetizers');
 
     const elements = [menuHeader, menuContainer];
     elements.forEach(el => {
